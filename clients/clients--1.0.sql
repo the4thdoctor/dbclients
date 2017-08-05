3142946880a5a7ac902ec5e8aa2349d32e398122
Sébastien Lardière <s.lardiere@loxodata.com>
2017-08-05 19:03:15 +0200

    Ajout de l'extension clients

---
+++ clients/clients--1.0.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION montant_ttc ( montant_ht numeric, tva numeric default 20.6 )
+RETURNS numeric
+LANGUAGE 'plpgsql'
+STABLE
+AS $$
+BEGIN
+    RETURN montant_ht * (1 + (tva / 100  )) ;
+END ;
+$$ ;
+
+CREATE OR REPLACE FUNCTION montant_facture (id_facture text)
+RETURNS numeric
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE
+  somme_ttc numeric ;
+BEGIN
+  RAISE NOTICE 'ID Facture : % ' , id_facture ;
+  SELECT montant_ttc(sum (lf_montant)) into somme_ttc
+    FROM lignes_factures
+    WHERE fact_num = id_facture ;
+ RAISE NOTICE 'Somme : % ' , somme_ttc ;
+RETURN somme_ttc ;
+END ;
+$$ ;
